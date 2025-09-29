c47d9aea3b28b0513864a81a7ad043f2a37035ad
2806762759@qq.com <2806762759@qq.com>
2025-09-29 23:27:35 +0800

    planning_process配置

--- src/planning/src/planning_process/planning_process.cpp
+++ src/planning/src/planning_process/planning_process.cpp
@@ -5,6 +5,13 @@ namespace Planning{
     PlanningProcess::PlanningProcess() : Node("planning_node")  // 规划总流程
     {
         RCLCPP_INFO(this->get_logger(),"planning_node created");
+
+        // read config
+        process_config_ = std::make_unique<ConfigReader>();
+        process_config_->read_planning_process_config();
+        auto obs_dis = process_config_->process().obs_dis_;
+        // RCLCPP_INFO(this->get_logger(),"obs_dis: %.2f",obs_dis);
+
     }
 
     bool PlanningProcess::process(){    // 总流程
--- src/planning/src/planning_process/planning_process.h
+++ src/planning/src/planning_process/planning_process.h
@@ -2,6 +2,18 @@
 #define PLANNING_PROCESS_H_
 
 #include "rclcpp/rclcpp.hpp"
+#include "config_reader.h"
+#include "main_car_info.h"
+#include "obs_car_info.h"
+#include "reference_line_creator.h"
+#include "decision_center.h"
+#include "local_path_planner.h"
+#include "local_speeds_planner.h"
+#include "local_trajectory_combiner.h"
+
+#include <vector>
+#include <cmath>
+#include <algorithm>
 
 namespace Planning
 {
@@ -12,7 +24,7 @@ namespace Planning
     bool process(); // 总流程
 
   private:
-
+    std::unique_ptr<ConfigReader> process_config_;
   };
 }  // namespace Planning
 #endif  // PLANNING_PROCESS_H_
