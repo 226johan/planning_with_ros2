6bdeac3fbfa9af4bb7fd201a797140396f027f2b
2806762759@qq.com <2806762759@qq.com>
2025-10-03 01:51:45 +0800

    [特性新增] global_path服务器

--- src/planning/src/global_planner/global_path_server.cpp
+++ src/planning/src/global_planner/global_path_server.cpp
@@ -1,15 +1,66 @@
-#include"global_path_server.h"
+#include "global_path_server.h"
 
-namespace Planning{
-    GlobalPathServer::GlobalPathServer() : Node("global_path_server_node") //全局路径服务器
+namespace Planning
+{
+    GlobalPathServer::GlobalPathServer() : Node("global_path_server_node") // 全局路径服务器
     {
         RCLCPP_INFO(this->get_logger(), "global_path_server_node created");
+
+        // 创建全局路径规划器
+        global_path_pub_ = this->create_publisher<Path>("global_path", 10);
+        global_path_rviz_pub_ = this->create_publisher<Marker>("global_path_rviz", 10);
+        // 创建全局路径服务
+        global_path_server_ = this->create_service<GlobalPathService>(
+            "global_path_service",
+            std::bind(&GlobalPathServer::reponse_global_path_callback, this, std::placeholders::_1, std::placeholders::_2));
     }
 
-}   //namespace Planning
+    void GlobalPathServer::reponse_global_path_callback(const std::shared_ptr<GlobalPathService::Request> request,
+                                                        const std::shared_ptr<GlobalPathService::Response> response)
+    {
+        // 接受请求，多态
+        switch (request->global_planner_type)
+        {
+        case static_cast<int>(GlobalPlannerType::NORMAL):
+            global_planner_base_ = std::make_shared<GlobalPathNormal>();
+            break;
 
-int main(int argc, char *argv[]){
-    rclcpp::init(argc,argv);
+        default:
+            RCLCPP_WARN(this->get_logger(), "Invalid global_planner type");
+            return;
+        }
+        // 判断请求是否有效
+        if (request->pnc_map.midline.points.empty())
+        {
+            RCLCPP_ERROR(this->get_logger(), "pnc_map empty, global_path connot be created");
+            return;
+        }
+        // 搜索并相应全局路径
+        const auto global_path = global_planner_base_->serch_global_path(request->pnc_map);
+        response->global_path = global_path;
+
+        // 发布全局路径，给局部规划用
+
+        global_path_pub_->publish(global_path);
+        RCLCPP_INFO(this->get_logger(), "global_path published");
+        // 发布用于rviz显示的全局路径
+        // bug修复：global_path 只发布一次 path没有frame_locked 无法固定在rviz中
+        const auto global_path_rviz = path2marker(global_path);
+        global_path_rviz_pub_->publish(global_path_rviz);
+        RCLCPP_INFO(this->get_logger(), "global_path for rviz published");
+    }
+
+    Marker GlobalPathServer::path2marker(const Path &path)
+    {
+        Marker path_rviz_;
+        return path_rviz_;
+    }
+
+} // namespace Planning
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
     auto node = std::make_shared<Planning::GlobalPathServer>();
     rclcpp::spin(node);
     rclcpp::shutdown();
--- src/planning/src/global_planner/global_path_server.h
+++ src/planning/src/global_planner/global_path_server.h
@@ -2,15 +2,32 @@
 #define GLOBAL_PATH_SERVER_H_
 
 #include "rclcpp/rclcpp.hpp"
-#include"global_planner_normal.h"
+#include "base_msgs/srv/global_path_service.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "visualization_msgs/msg/marker.hpp"
+#include "global_planner_normal.h"
 
 namespace Planning
 {
-    class GlobalPathServer : public rclcpp::Node  // 全局路径规划器基类
+    using base_msgs::srv::GlobalPathService;
+    using geometry_msgs::msg::Point;
+    using visualization_msgs::msg::Marker;
+    class GlobalPathServer : public rclcpp::Node // 全局路径规划器基类
     {
-        public:
+    public:
         GlobalPathServer();
 
+    private:
+        // 全局路径回调
+        void reponse_global_path_callback(const std::shared_ptr<GlobalPathService::Request> request,
+                                          const std::shared_ptr<GlobalPathService::Response> response);
+        Marker path2marker(const Path &path); // path转marker
+
+    private:
+        std::shared_ptr<GlobalPlannerBase> global_planner_base_;           // 全局路径创建器
+        rclcpp::Publisher<Path>::SharedPtr global_path_pub_;               // 全局路径发布器
+        rclcpp::Publisher<Marker>::SharedPtr global_path_rviz_pub_;        // 全局路径marker发布器
+        rclcpp::Service<GlobalPathService>::SharedPtr global_path_server_; // 全局路径服务器
     };
-}  // namespace Planning
-#endif  // GLOBAL_PATH_SERVER_H_
+} // namespace Planning
+#endif // GLOBAL_PATH_SERVER_H_
