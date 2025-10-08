4bd1756f5798b45abada070115a7e7665af2a567
2806762759@qq.com <2806762759@qq.com>
2025-10-08 19:11:55 +0800

    [问题修复] 地图方向bug修复

--- src/planning/src/pnc_map_creator/pnc_map_straight/pnc_map_straight.cpp
+++ src/planning/src/pnc_map_creator/pnc_map_straight/pnc_map_straight.cpp
@@ -85,7 +85,7 @@ namespace Planning
 
             len_tmp += len_step_ * ratio;
             // p_mid_.x += len_tmp * plus_flag;
-            p_mid_.x += len_step_ * plus_flag;
+            p_mid_.x += len_step_ * plus_flag * ratio;
         }
     }
 
