db0ae8931d8ea6a3186bba758998eea6b9445c76
2806762759@qq.com <2806762759@qq.com>
2025-10-08 23:02:15 +0800

    [特性新增] 添加初始化全局路径功能

--- src/planning/src/global_planner/global_planner_normal/global_planner_normal.h
+++ src/planning/src/global_planner/global_planner_normal/global_planner_normal.h
@@ -1,18 +1,19 @@
 #ifndef GLOBAL_PLANNER_NORMAL_H_
 #define GLOBAL_PLANNER_NORMAL_H_
 
-#include"global_planner_base.h"
+#include "global_planner_base.h"
 
 namespace Planning
 {
-class GlobalPathNormal : public GlobalPlannerBase  // 普通全局路径规划器
-{
-public:
-    GlobalPathNormal();
-    Path serch_global_path(const PNCMap &pnc_map) override;
+    class GlobalPathNormal : public GlobalPlannerBase // 普通全局路径规划器
+    {
+    public:
+        GlobalPathNormal();
+        Path serch_global_path(const PNCMap &pnc_map) override;
 
-private:
-
-};
-}  // namespace Planning
-#endif  // GLOBAL_PLANNER_NORMAL_H_
+    private:
+    public:
+        void init_global_path(const PNCMap &pnc_map);
+    };
+} // namespace Planning
+#endif // GLOBAL_PLANNER_NORMAL_H_
