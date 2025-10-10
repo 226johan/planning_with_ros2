624cf927331e7badd383e87866eb9976272edac6
2806762759@qq.com <2806762759@qq.com>
2025-10-11 01:18:13 +0800

    [特性新增] 实现曲线匹配点查找与投影参数计算功能

--- src/planning/src/common/math/curve.cpp
+++ src/planning/src/common/math/curve.cpp
@@ -1,5 +1,143 @@
-#include"curve.h"
+#include "curve.h"
 
-namespace Planning{
-    
+namespace Planning
+{
+    int Curve::find_match_point(const Path &path, const int &last_match_point_index, const PoseStamped &target_point)
+    {
+        const int path_size = path.poses.size();
+        if (path_size <= 1)
+        {
+            return path_size - 1;
+        }
+        double min_dis = std::numeric_limits<double>::max();
+        int closet_index = -1;
+        const int threshould_jump{100};
+        for (int i = 0; i < path_size; i++)
+        {
+            double dis = std::hypot(path.poses[i].pose.position.x - target_point.pose.position.x,
+                                    path.poses[i].pose.position.y - target_point.pose.position.y);
+            if (dis < min_dis)
+            {
+                if (abs(i - last_match_point_index) > threshould_jump)
+                {
+                    continue;
+                }
+                min_dis = dis;
+                closet_index = i;
+            }
+        }
+
+        return closet_index;
+    }
+    void Curve::cal_projection_param(Referline &refer_line)
+    {
+        const int path_size = refer_line.refer_line.size();
+        if (path_size < 3)
+        {
+            RCLCPP_ERROR(rclcpp::get_logger("math"), "refer_line too short");
+            return;
+        }
+        // 计算rs
+        double rs{0.0};
+        for (int i = 0; i < path_size; i++)
+        {
+            if (i == 0)
+            {
+                rs = 0.0;
+            }
+            else
+            {
+                // 用两点距离近似累积弧线
+                rs += std::hypot(refer_line.refer_line[i].pose.pose.position.y - refer_line.refer_line[i - 1].pose.pose.position.y,
+                                 refer_line.refer_line[i].pose.pose.position.x - refer_line.refer_line[i - 1].pose.pose.position.x);
+            }
+            refer_line.refer_line[i].rs = rs;
+        }
+        // 计算航向角 theta
+        for (int i = 0; i < path_size; i++)
+        {
+            if (i < path_size - 1)
+            {
+                refer_line.refer_line[i].rtheta =
+                    std::atan2(refer_line.refer_line[i + 1].pose.pose.position.y - refer_line.refer_line[i].pose.pose.position.y,
+                               refer_line.refer_line[i + 1].pose.pose.position.x - refer_line.refer_line[i].pose.pose.position.x);
+            }
+            else
+            {
+                // 最后一个点用前一个点计算
+                refer_line.refer_line[i].rtheta =
+                    std::atan2(refer_line.refer_line[i].pose.pose.position.y - refer_line.refer_line[i - 1].pose.pose.position.y,
+                               refer_line.refer_line[i].pose.pose.position.x - refer_line.refer_line[i - 1].pose.pose.position.x);
+            }
+            refer_line.refer_line[i].rs = rs;
+        }
+        // 计算曲率   theta[i+1]-theta[i] / ds
+        for (int i = 0; i < path_size; i++)
+        {
+            if (i < path_size - 1)
+            {
+                const double dis = std::hypot(refer_line.refer_line[i + 1].pose.pose.position.y - refer_line.refer_line[i].pose.pose.position.y,
+                                              refer_line.refer_line[i + 1].pose.pose.position.x - refer_line.refer_line[i].pose.pose.position.x);
+                if (dis < kMathEpsilon)
+                {
+                    refer_line.refer_line[i].rkappa = 0.0;
+                    continue;
+                }
+                else
+                {
+                    refer_line.refer_line[i].rkappa = (refer_line.refer_line[i + 1].rtheta - refer_line.refer_line[i].rtheta) / dis;
+                }
+            }
+            else
+            {
+                // 最后一个点用前一个点计算
+                const double dis = std::hypot(refer_line.refer_line[i].pose.pose.position.y - refer_line.refer_line[i - 1].pose.pose.position.y,
+                                              refer_line.refer_line[i].pose.pose.position.x - refer_line.refer_line[i - 1].pose.pose.position.x);
+                if (dis < kMathEpsilon)
+                {
+                    refer_line.refer_line[i].rkappa = 0.0;
+                    continue;
+                }
+                else
+                {
+                    refer_line.refer_line[i].rkappa = (refer_line.refer_line[i].rtheta - refer_line.refer_line[i - 1].rtheta) / dis;
+                }
+            }
+        }
+
+        // 计算曲率变化率
+
+        for (int i = 0; i < path_size; i++)
+        {
+            if (i < path_size - 1)
+            {
+                const double dis = std::hypot(refer_line.refer_line[i + 1].pose.pose.position.y - refer_line.refer_line[i].pose.pose.position.y,
+                                              refer_line.refer_line[i + 1].pose.pose.position.x - refer_line.refer_line[i].pose.pose.position.x);
+                if (dis < kMathEpsilon)
+                {
+                    refer_line.refer_line[i].rdkappa = 0.0;
+                    continue;
+                }
+                else
+                {
+                    refer_line.refer_line[i].rdkappa = (refer_line.refer_line[i + 1].rkappa - refer_line.refer_line[i].rkappa) / dis;
+                }
+            }
+            else
+            {
+                // 最后一个点用前一个点计算
+                const double dis = std::hypot(refer_line.refer_line[i].pose.pose.position.y - refer_line.refer_line[i - 1].pose.pose.position.y,
+                                              refer_line.refer_line[i].pose.pose.position.x - refer_line.refer_line[i - 1].pose.pose.position.x);
+                if (dis < kMathEpsilon)
+                {
+                    refer_line.refer_line[i].rdkappa = 0.0;
+                    continue;
+                }
+                else
+                {
+                    refer_line.refer_line[i].rdkappa = (refer_line.refer_line[i].rkappa - refer_line.refer_line[i - 1].rkappa) / dis;
+                }
+            }
+        }
+    }
 }
--- src/planning/src/common/math/curve.h
+++ src/planning/src/common/math/curve.h
@@ -2,15 +2,30 @@
 #define CURVE_H_
 
 #include "rclcpp/rclcpp.hpp"
+#include"base_msgs/msg/referline.hpp"
+#include"base_msgs/msg/local_path.hpp"
+#include"nav_msgs/msg/path.hpp"
+#include"geometry_msgs/msg/pose_stamped.hpp"
 #include<cmath>
 
 namespace Planning
 {
+    using geometry_msgs::msg::PoseStamped;
+    using base_msgs::msg::Referline;
+    using base_msgs::msg::LocalPath;
+    using nav_msgs::msg::Path;
+    constexpr double kMathEpsilon = 1e-6;
     class Curve // 曲线
     {
     public:
+
         Curve() = default;
 
+        // 找匹配点下标
+        static int find_match_point(const Path &path,const int &last_match_point_index,const PoseStamped &target_point);
+
+        // 计算投影点参数
+        static void cal_projection_param(Referline &refer_line); // 参考线
     };
 }  // namespace Planning
 #endif  // CURVE_H_
